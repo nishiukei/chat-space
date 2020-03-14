cf189bd69d4e5c9ec17abe2b545297137f7c39c5
nishiukei <nisiukei@yahoo.co.jp>
2020-03-14 16:01:19 +0900

    group edit&create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "groups#index"
+  resources :messages, only:[:index]
   resources :users, only:[:edit, :update, :destroy]
+  resources :groups, only:[:index, :new, :create, :edit, :update]
 end
