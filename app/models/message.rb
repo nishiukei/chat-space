2d135e62ab2adec3f2b7039e2784addb1b4f9acb
nishiukei <nisiukei@yahoo.co.jp>
2020-03-18 00:22:57 +0900

    link create

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,8 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  validates :contet, presence: true, unless: :image?
+  validates :content, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
+
 end
