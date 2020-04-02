da8a8367e80c0872706fdb40128dc8f0269d0f5d
nishiukei <nisiukei@yahoo.co.jp>
2020-04-02 20:00:59 +0900

    Revert "Revert " Automatic updating""
    This reverts commit 3629090a8367abaf9293300ee51e54aae7144157.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,9 @@
 $(function() {
 
   function buildHTML(message){
-    if (message.image) {
+    if (message.content && message.image) {
       var html =
-        `<div class="main_chat__list__box">
+        `<div class="main_chat__list__box" data-message-id=${message.id}>
             <div class="main_chat__list__box__user">
               <div class="user">
                 <b>
@@ -19,10 +19,9 @@ $(function() {
             <img class="main_chat_image" src=${message.image}>
           </div>
         </div>`
-      return html;
-    } else {
+    } else if (message.content) {
       var html = 
-        `<div class="main_chat__list__box">
+        `<div class="main_chat__list__box" data-message-id=${message.id}>
             <div class="main_chat__list__box__user">
               <div class="user">
                 <b>
@@ -37,13 +36,29 @@ $(function() {
             ${message.content}
           </div>
         </div>`
-      return html;
+    } else if (message.image) {
+      var html = 
+        `<div class="main_chat__list__box" data-message-id=${message.id}>
+          <div class="main_chat__list__box__user">
+            <div class="user">
+              <b>
+                ${message.user_name}
+              </b>
+            </div>
+            <div class="time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="main_chat__list__message">
+            <img class="main_chat_image" src=${message.image}>
+          </div>
+        </div>`
     };
-  }
+    return html;
+  };
 
   $("#new_message").on('submit', function(e) {
     e.preventDefault();
-    console.log(1)
     var formData = new FormData(this);
     var url = $(this).attr("action");
     $.ajax({
@@ -65,4 +80,30 @@ $(function() {
       alert("メッセージ送信に失敗しました");
     });
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $(".main_chat__list__box:last").data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: "get",
+      dataType: "json",
+      data: { id: last_message_id }
+    })
+    .done(function(messages){
+      if (messages.length !== 0) {
+        var insertHTML = "";
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $(".main_chat__list").append(insertHTML);
+        $(".main_chat__list").animate({ scrollTop: $(".main_chat__list")[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      alert("error"); 
+    })
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
